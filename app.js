2796185e2fffbe67e8728ca689152641f42d043d
frayfray248 <nicman248@hotmail.com>
2021-02-17 17:26:14 -0800

    basic socket server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // modules
 const express = require('express');
 const http = require('http');
+const socketIO = require("socket.io");
 
 // dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -13,12 +14,28 @@ const PORT = process.env.PORT;
 // objects
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 
 // root
 app.use('/', (req, res) => {
-    res.status(200).send("hello world");
+    res.status(200).send(`
+    <h1>Hello World<h1>
+    <script src="/socket.io/socket.io.js"></script>
+    <script>
+        var socket = io();
+    </script>
+    `);
 });
+
+// socket handlers
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+})
 
 // server start
 server.listen(PORT, console.log(`Listening on port ${PORT}`));
